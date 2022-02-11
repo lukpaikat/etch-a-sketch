8cee5c853ea9feb257f8c261ee252d029b73a9de
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-11 17:10:09 +0800

    add createGrid function

---
+++ scripts/utils/create-grid.js
@@ -0,0 +1,34 @@
+import colorBlock from './block-color.js';
+import config from '../etc/config.js';
+
+const createGrid = (gridSize) => {
+  const gridContainer = document.querySelector('.grid-container');
+  let mousedown = false;
+  gridContainer.onmousedown = () => mousedown = true;
+  gridContainer.onmouseup = () => mousedown = false;
+  gridContainer.innerHTML = '';
+  const targetTotalItem = gridSize ** 2;
+  let currentTotalItem = 0;
+  while (currentTotalItem < targetTotalItem) {
+    const item = document.createElement('div');
+    item.classList.add('grid-item');
+    item.onmouseover = (e) => {
+      if (mousedown) {
+        colorBlock({
+          element: e.target,
+          color: config.color
+        });
+      }
+    };
+    item.onmousedown = (e) => {
+      colorBlock({
+        element: e.target,
+        color: config.color
+      });
+    };
+    gridContainer.appendChild(item);
+    currentTotalItem = currentTotalItem + 1;
+  }
+}
+
+export default createGrid;
