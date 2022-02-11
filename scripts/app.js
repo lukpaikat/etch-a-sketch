ec7d31d8aa4d464b483f613b16874d5dc0f9632a
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-11 17:10:34 +0800

    add app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,7 @@
-let mousedown = false;
-document.body.onmousedown = () => mousedown = true;
-document.body.onmouseup = () => mousedown = false;
+import createGrid from './utils/create-grid.js'
+import clearGrid from './utils/clear-grid.js';
 
-const blockColor = (element) => {
-  const thisBlock = element;
-  thisBlock.style.backgroundColor = '#000000';
-}
+const clearButton = document.getElementById('clear-button');
 
-const initiateGrid = () => {
-  const gridContainer = document.querySelector('.grid-container');
-  const targetTotalItem = 16 * 16;
-  let currentTotalItem = 0;
-  while (currentTotalItem < targetTotalItem) {
-    const item = document.createElement('div');
-    item.classList.add('grid-item');
-    item.onmouseover = (e) => {
-      if (mousedown) {
-        blockColor(e.target);
-      }
-    };
-    item.onmousedown = (e) => {
-      blockColor(e.target);
-    };
-    gridContainer.appendChild(item);
-    currentTotalItem = currentTotalItem + 1;
-  }
-}
-
-initiateGrid();
+clearButton.onclick = clearGrid;
+createGrid(16);
