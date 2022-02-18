e4d30c6dd52dab3da92cf3e862b9ff16e0cba98c
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-18 15:39:29 +0800

    add eraser function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 import createGrid from './utils/create-grid.js'
 import clearGrid from './utils/clear-grid.js';
 import randomColor from './utils/random-color.js';
+import outlineGrid from './utils/outline-grid.js';
 import config from './etc/config.js';
 
 const clearButton = document.getElementById('clear-button');
@@ -12,6 +13,12 @@ colorButton.onchange = (e) => { config.color = () => e.target.value; };
 
 const randomButton = document.getElementById('random-button');
 randomButton.onclick = () => { config.color = () => randomColor() }
+
+const eraserButton = document.getElementById('eraser-button');
+eraserButton.onclick = () => { config.color = () => '#fff'}
+
+const gridButton = document.getElementById('grid-button');
+gridButton.onclick = outlineGrid;
 
 const gridSize = document.getElementById('grid-size');
 gridSize.onchange = (e) => createGrid(e.target.value)
