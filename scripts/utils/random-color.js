4b503cf396112f54f2affb472941236a906adff3
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-18 15:00:32 +0800

    add random color function

---
+++ scripts/utils/random-color.js
@@ -0,0 +1,8 @@
+const randomColor = () => {
+  const R = Math.floor(Math.random() * 256);
+  const G = Math.floor(Math.random() * 256);
+  const B = Math.floor(Math.random() * 256);
+  return `rgb(${R}, ${G}, ${B})`
+}
+
+export default randomColor;
