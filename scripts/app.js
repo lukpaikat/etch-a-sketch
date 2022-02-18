4f09fed0d4c323aae8c3a7a1abaa9fd321f5b876
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-18 15:02:13 +0800

    change and add evenlistener to anonymous function

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,21 @@
 import createGrid from './utils/create-grid.js'
 import clearGrid from './utils/clear-grid.js';
+import randomColor from './utils/random-color.js';
 import config from './etc/config.js';
 
 const clearButton = document.getElementById('clear-button');
 clearButton.onclick = clearGrid;
 
 const colorButton = document.getElementById('pen');
-colorButton.onclick = (e) => {
-  config.color = e.target.value
-}
-colorButton.onchange = (e) => {
-  config.color = e.target.value;
-}
+colorButton.onclick = (e) => { config.color = () => e.target.value; };
+colorButton.onchange = (e) => { config.color = () => e.target.value; };
+
+const randomButton = document.getElementById('random-button');
+randomButton.onclick = () => { config.color = () => randomColor() }
 
 const gridSize = document.getElementById('grid-size');
 gridSize.onchange = (e) => createGrid(e.target.value)
 
 createGrid(16);
+
+console.log(randomColor())
