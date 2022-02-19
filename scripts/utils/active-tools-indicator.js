87dc43a338999f873c074aa6754686bee59df16c
lukpaikat <mhasbisaputra5@gmail.com>
2022-02-19 19:41:40 +0800

    remove  activeToolIndicator iterator

--- scripts/utils/active-tools-indicator.js
+++ scripts/utils/active-tools-indicator.js
@@ -1,9 +1,7 @@
-const activeToolsIndicator = (activeButton) => {
-  const tools = document.getElementsByClassName('tool-button');
-  for (const tool of tools) {
-    tool.classList.remove('active-tool');
-  }
-  activeButton.classList.add('active-tool');
+const activeToolsIndicator = (activeTool) => {
+  const lastActiveTool = document.querySelector('.active-tool');
+  lastActiveTool.classList.remove('active-tool');
+  activeTool.classList.add('active-tool');
 }
 
 export default activeToolsIndicator;
